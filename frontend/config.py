8238b9e85a07f7cfdaafa9280c1966858c177a28
LeoBallari <LeandroBallari@gmail.com>
2026-06-05 20:00:50 -0300

    punto de econtrol

--- frontend/config.py
+++ frontend/config.py
@@ -10,7 +10,7 @@ ASSETS_DIR = os.path.join(BASE_DIR, "assets")
 # API
 API_URL = "https://amincamponuevo2026movil.onrender.com"
 API_TIMEOUT = 60.0
-MAPS_API_KEY = "AIzaSyD8X-livltHmMHWIMaJGnTmIF7SZS5AizA"
+MAPTILER_API_KEY = "tn7F07rMO2N0LVRlRUmf"
 
 # Configuración de ventana
 WINDOW_WIDTH = 380
